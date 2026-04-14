45859275cb04c3211490f0e891dcab52fb9abbf4
rriazn <rnriazanov@gmail.com>
2026-04-14 15:54:34 +0200

    Move All tests to separate jobs

--- tests/api/exercises_test.py
+++ tests/api/exercises_test.py
@@ -3,5 +3,5 @@ from models import Exercise
 
 def test_list_exercises(client: TestClient, token: str, exercise: Exercise):
     response = client.get("/api/exercises", headers={"Authorization": f"Bearer {token}"})
-    assert response.status_code != 200
+    assert response.status_code == 200
     assert any(e["id"] == exercise.id for e in response.json()["exercises"]), "Created exercise should be in the list"
